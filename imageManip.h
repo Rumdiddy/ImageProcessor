4a8829ca1393494ce827927ab10b579a5ed3e8c6
Samuel Jin <sjin16@jhu.edu>
2020-03-18 23:08:59 +0000

    Fixed merger error

--- imageManip.h
+++ imageManip.h
@@ -6,20 +6,10 @@
 //imageManip.h
 //Headerfile for all the operations functions
 
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-
 //performs exposure functin on input image
 //returns output image pointer. 
 Image * exposure(Image *input, Image *output, factor);
-=======
+
 //Performs zoomin function on the provided image
 //writes output into empty output image.
 Image * zoomIN(Image *input, Image *output);
->>>>>>> 445964bf7d04d2b6bf13633f887b4e3e10fc16a7
-=======
-//Performs zoomin function on the provided image
-//writes output into empty output image.
-Image * zoomIN(Image *input, Image *output);
-bf7d04d2b6bf13633f887b4e3e10fc16a7
