467d90f25acaf326e9062583b6e928d1ae8e29b5
Ajay Ananthakrishnan <ajayananth@jhu.edu>
2020-03-18 19:15:06 -0400

    added another function

--- imageManip.h
+++ imageManip.h
@@ -8,8 +8,11 @@
 
 //performs exposure functin on input image
 //returns output image pointer. 
-Image * exposure(Image *input, Image *output, factor);
+Image * exposure(Image *input, Image *output, double factor);
 
 //Performs zoomin function on the provided image
 //writes output into empty output image.
 Image * zoomIN(Image *input, Image *output);
+
+//
+Image *blend(Image *imput1, Image *input2, Image *output, double factor ); 
