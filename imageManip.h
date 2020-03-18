688f3b9b7797199fda4de8f79ef42d18ee648e01
Ajay Ananthakrishnan <ajayananth@jhu.edu>
2020-03-18 18:49:04 -0400

    imageManip.h

--- imageManip.h
+++ imageManip.h
@@ -6,7 +6,13 @@
 //imageManip.h
 //Headerfile for all the operations functions
 
+<<<<<<< HEAD
 
 //performs exposure functin on input image
 //returns output image pointer. 
 Image * exposure(Image *input, Image *output, factor);
+=======
+//Performs zoomin function on the provided image
+//writes output into empty output image.
+Image * zoomIN(Image *input, Image *output);
+>>>>>>> 445964bf7d04d2b6bf13633f887b4e3e10fc16a7
