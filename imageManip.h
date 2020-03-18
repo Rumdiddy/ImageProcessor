a24262bd4890794f2eba0ddc6827d96228fca1a1
Ajay Ananthakrishnan <ajayananth@jhu.edu>
2020-03-18 18:45:31 -0400

    completed 1 function in header

--- imageManip.h
+++ imageManip.h
@@ -6,3 +6,7 @@
 //imageManip.h
 //Headerfile for all the operations functions
 
+
+//performs exposure functin on input image
+//returns output image pointer. 
+Image * exposure(Image *input, Image *output, factor);
