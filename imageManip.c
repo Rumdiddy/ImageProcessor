94409887d1b214ccbe528a96b882b4ab6f0c99fa
Ajay Ananthakrishnan <ajayananth@jhu.edu>
2020-03-19 22:07:16 -0400

    made header functions void and worked on exposure and zoomIN function

--- imageManip.c
+++ imageManip.c
@@ -8,3 +8,31 @@
 
 #include "imageManip.h"
 
+
+void exposure(Image *input, Image *output, double factor){
+
+  inputLength = (*input -> rows) * (*input -> cols); 
+  for (int i = 0; i < inputLength; i++){
+    *(output -> data + i) = *(input -> data + i) * pow(2, factor); 
+  }
+}
+
+
+void zoomIN(Image *input, Image *output){
+
+  inputLength =	(*input -> rows) * (*input -> cols);
+  
+  for (int i = 0; i < inputLength; i++){
+    
+    for (int j = 0; j < 4; j++){
+
+      if (i>1){
+        *(output -> data + i + (*input ->rows) +j - 2) = *(input -> data + i);
+      }
+      else{
+      *(output -> data + i +j) = *(input -> data + i);
+      }
+    }
+  }
+
+}
