03a1a16df753e0ff42dca635de4e1f46161b1aad
Samuel Jin <sjin16@jhu.edu>
2020-03-21 22:10:04 +0000

    Added comments and fixed typos and formatting

--- imageManip.c
+++ imageManip.c
@@ -22,7 +22,6 @@ void exposure(Image *input, Image *output, double factor){
   }
 }
 
-
 //Performs blending of two image files by a certain factor              
 void blend(Image *imput1, Image *input2, Image *output, double factor ){
 
@@ -32,43 +31,44 @@ void blend(Image *imput1, Image *input2, Image *output, double factor ){
   int colTracker = 1; //what col you are in 
   
   for (int i = 0; i < outputLength; i++){
-
-    if (colTracker > (input1 -> cols)){           //case where input 2 has more cols than input 1
+    
+    //case where input 2 has more cols than input 1
+    if (colTracker > (input1 -> cols)){          
       ((output -> data + i) -> r) = ((input2 -> data + i) -> r);
       ((output -> data + i) -> g) = ((input2 -> data + i) -> g);
       ((output -> data + i) -> b) = ((input2 -> data + i) -> b);
-
     }
     
-    else if (colTracker > (input2 -> cols)){      //vice versa case
+    //vice versa case
+    else if (colTracker > (input2 -> cols)){      
       ((output -> data + i) -> r) = ((input1 -> data + i) -> r);
       ((output -> data + i) -> g) = ((input1 -> data + i) -> g);
       ((output -> data + i) -> b) = ((input1 -> data + i) -> b);
-
     }
     
-    else if (rowTracker > (input1 -> rows)){  	  //case where input 2 has more rows than input 1                     
+    //case where input 2 has more rows than input 1
+    else if (rowTracker > (input1 -> rows)){  	                       
       ((output -> data + i) -> r) = ((input2 -> data + i) -> r);
       ((output -> data + i) -> g) = ((input2 -> data + i) -> g);
       ((output -> data + i) -> b) = ((input2 -> data + i) -> b);
-
     }
     
-    else if (rowTracker > (input2 -> rows)){      //vice versa case                                                   
+    //vice versa case
+    else if (rowTracker > (input2 -> rows)){                                                         
       ((output -> data + i) -> r) = ((input1 -> data + i) -> r);
       ((output -> data + i) -> g) = ((input1 -> data + i) -> g);
       ((output -> data + i) -> b) = ((input1 -> data + i) -> b);
-
     }
     
-    else{                                        //case when output index is within bounds of both inputs
+    //case when output index is within bounds of both inputs
+    else{                                       
     ((output -> data + i) -> r) = factor *  ((input1 -> data + i) -> r) + (1 - factor) * ((input2 -> data + i) -> r);
     ((output -> data + i) -> g) = factor *  ((input1 -> data + i) -> g) + (1 - factor) * ((input2 -> data + i) -> g);
     ((output -> data + i) -> b) = factor *  ((input1 -> data + i) -> b) + (1 - factor) * ((input2 -> data + i) -> b);
     }
-   
+     
+    //tracks when to skip over a row
     colTracker++;
-    
     if (colTracker == (*output.cols)){
       colTracker = 1;
       rowTracker++; 
@@ -77,28 +77,23 @@ void blend(Image *imput1, Image *input2, Image *output, double factor ){
   
 }
 
-
 //implementation of the zoomin function which zooms in on the input image by a factor of 2
 //and produces an output image 4 times the area.
 void zoomIN(Image *input, Image *output){
 
   int inputLength = (*input.rows) * (*input.cols);
-  
   int outind = 0;
   int rowskip = 0;
   
   //Iterates through the output and input image.
   for (int i = 0; i < inputLength; i++){ 
-   
-   for (int j = 0; j < 4; j++){
-    
+    for (int j = 0; j < 4; j++){
       if (j > 1){
         *(output -> data + (rowskip * (*input.cols)) + (2 * i) + (*input.cols) + (j - 2)).r = *(input -> data + i).r;
         *(output -> data + (rowskip * (*input.cols)) + (2 * i) + (*input.cols) + (j - 2)).g = *(input -> data + i).g;
         *(output -> data + (rowskip * (*input.cols)) + (2 * i) + (*input.cols) + (j - 2)).b = *(input -> data + i).b;
-      }
-      else{
-	*(output -> data + (rowskip * (*input.cols)) + (2 * i) +j).r = *(input -> data + i).r;
+      } else {
+	    *(output -> data + (rowskip * (*input.cols)) + (2 * i) +j).r = *(input -> data + i).r;
         *(output -> data + (rowskip * (*input.cols)) + (2 * i) +j).g = *(input -> data + i).g;
         *(output -> data + (rowskip * (*input.cols)) + (2 * i) +j).b = *(input -> data + i).b;
       }
@@ -111,27 +106,27 @@ void zoomIN(Image *input, Image *output){
   }
 }
 
-
 //Performs zoomout function on the provided image                       
 void zoomOUT(Image * input, Image *output){
 
   int rowskip = 0; 
-  int inputLength = (*output.rows) * (*output.cols);
-
-  for (int i = 0 ; i <inputLength; i++){
+  int outputLength = (*output.rows) * (*output.cols);
+  
+  //Loops through each pixel and condenses the average of 4 pixels into one pixel for color
+  for (int i = 0 ; i < outputLength; i++) {
     
-    *(output -> data + i).r = (*(input -> (data + (2 * i) + rowskip)).r + *(data + (2 * i) +1 + rowskip).r + *(data + (2 * i) + (*input.cols) + rowskip).r + *(data + (2 * i) + (*input.cols) + 1 + rowskip).r) / 4;
+    *(output -> data + i).r = (*(input -> (data + (2 * i) + rowskip)).r + *(data + (2 * i) +1 + rowskip).r + *(data + (2 *\
+    i) + (*input.cols) + rowskip).r + *(data + (2 * i) + (*input.cols) + 1 + rowskip).r) / 4;
 
     *(output -> data + i).g = (*(input -> (data + (2 * i) + rowskip)).g + *(data + (2 * i) +1 + rowskip).g + *(data + (2 *\
- i) + (*input.cols) + rowskip).g + *(data + (2 * i) + (*input.cols) + 1 + rowskip).g) / 4;
+    i) + (*input.cols) + rowskip).g + *(data + (2 * i) + (*input.cols) + 1 + rowskip).g) / 4;
 
     *(output -> data + i).b = (*(input -> (data + (2 * i) + rowskip)).b + *(data + (2 * i) +1 + rowskip).b + *(data + (2 *\
- i) + (*input.cols) + rowskip).b + *(data + (2 * i) + (*input.cols) + 1 + rowskip).b) / 4;
+    i) + (*input.cols) + rowskip).b + *(data + (2 * i) + (*input.cols) + 1 + rowskip).b) / 4;
     
     if (i % (*output.cols) = (*output.cols) - 1){
       rowskip = rowskip + (*input.cols); 
     }
-
   }
 }
 
