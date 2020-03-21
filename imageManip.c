128cbd976878de70506ac5019b56c3e83dfb5658
Ajay Ananthakrishnan <ajayananth@jhu.edu>
2020-03-21 16:42:23 -0400

    fixed error in imageManip

--- imageManip.c
+++ imageManip.c
@@ -15,7 +15,10 @@ void exposure(Image *input, Image *output, double factor){
 
   int inputLength = (*input.rows) * (*input.cols); 
   for (int i = 0; i < inputLength; i++){
-    *(output -> data + i) = *(input -> data + i) * pow(2, factor); 
+    *(output -> data + i).r = *(input -> data + i) * pow(2, factor);
+    *(output -> data + i).g = *(input -> data + i) * pow(2, factor);
+    *(output -> data + i).b = *(input -> data + i) * pow(2, factor);
+
   }
 }
 
@@ -23,7 +26,7 @@ void exposure(Image *input, Image *output, double factor){
 //and produces an output image 4 times the area.
 void zoomIN(Image *input, Image *output){
 
-  int inputLength =	(*input.rows) * (*input.cols);
+  int inputLength = (*input.rows) * (*input.cols);
   
   int outind = 0;
   int rowskip = 0;
@@ -34,10 +37,14 @@ void zoomIN(Image *input, Image *output){
    for (int j = 0; j < 4; j++){
     
       if (j > 1){
-        *(output -> data + (rowskip * (*input.cols)) + (2 * i) + (*input.cols) + (j - 2)) = *(input -> data + i);
+        *(output -> data + (rowskip * (*input.cols)) + (2 * i) + (*input.cols) + (j - 2)).r = *(input -> data + i).r;
+        *(output -> data + (rowskip * (*input.cols)) + (2 * i) + (*input.cols) + (j - 2)).g = *(input -> data + i).g;
+        *(output -> data + (rowskip * (*input.cols)) + (2 * i) + (*input.cols) + (j - 2)).b = *(input -> data + i).b;
       }
       else{
-      *(output -> data + (rowskip * (*input.cols)) + (2 * i) +j) = *(input -> data + i);
+	*(output -> data + (rowskip * (*input.cols)) + (2 * i) +j).r = *(input -> data + i).r;
+        *(output -> data + (rowskip * (*input.cols)) + (2 * i) +j).g = *(input -> data + i).g;
+        *(output -> data + (rowskip * (*input.cols)) + (2 * i) +j).b = *(input -> data + i).b;
       }
     }
     
@@ -54,13 +61,21 @@ void zoomOUT(Image * input, Image *output){
   int inputLength = (*output.rows) * (*output.cols);
 
   for (int i = 0 ; i <inputLength; i++){
-    //this is actually wrong 
-    *(output -> data + i) = (*(input -> (data + (2 * i) + rowskip)) + *(data + (2 * i) +1 + rowskip) + *(data + (2 * i) + (*input.cols) + rowskip) + *(data + (2 * i) + (*input.cols) + 1 + rowskip)) / 4;   
+    
+    *(output -> data + i).r = (*(input -> (data + (2 * i) + rowskip)).r + *(data + (2 * i) +1 + rowskip).r + *(data + (2 * i) + (*input.cols) + rowskip).r + *(data + (2 * i) + (*input.cols) + 1 + rowskip).r) / 4;
 
+    *(output -> data + i).g = (*(input -> (data + (2 * i) + rowskip)).g + *(data + (2 * i) +1 + rowskip).g + *(data + (2 *\
+ i) + (*input.cols) + rowskip).g + *(data + (2 * i) + (*input.cols) + 1 + rowskip).g) / 4;
+
+    *(output -> data + i).b = (*(input -> (data + (2 * i) + rowskip)).b + *(data + (2 * i) +1 + rowskip).b + *(data + (2 *\
+ i) + (*input.cols) + rowskip).b + *(data + (2 * i) + (*input.cols) + 1 + rowskip).b) / 4;
+    
     if (i % (*output.cols) = (*output.cols) - 1){
       rowskip = rowskip + (*input.cols); 
     }
 
   }
 }
+
+
 
